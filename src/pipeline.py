e97f2b07c66cc3137a55f839f635021fda6dc559
Owen O'Connor <ooconnor@bu.edu>
2024-08-28 13:59:10 -0400

    pipeline.py works with CLI

--- src/pipeline.py
+++ src/pipeline.py
@@ -16,25 +16,24 @@ from trackformer.engine import pipeline
 from trackformer.models import build_model
 from trackformer.util.misc import nested_dict_to_namespace
 
-dataset = 'moma'
-dataset = 'DynamicNuclearNet-tracking-v1_0'
-
-modelpath = Path('/projectnb/dunlop/ooconnor/MOT/models/cell-trackformer/results') / dataset
-datapath = Path('/projectnb/dunlop/ooconnor/MOT/data') / dataset / 'CTC' / 'test'
+filepath = Path(__file__)
+yaml_file_paths = (filepath.parents[1] / 'cfgs').glob("*.yaml")
+yaml_files = [yaml_file.stem.split('_')[1] for yaml_file in yaml_file_paths]
 
 ex = sacred.Experiment('pipeline')
-ex.add_config(modelpath.as_posix() + '/config.yaml')
 
-def train(args: Namespace, modelpath, datapath) -> None:
+def train(args: Namespace, datapath) -> None:
 
-    args.output_dir = Path(args.output_dir)
     print(args.output_dir)
-    args.resume = modelpath / 'checkpoint.pth'
+    args.resume = args.output_dir / 'checkpoint.pth'
 
     dataset_name = args.dataset
 
     if not datapath.exists():
         datapath = datapath.parent / 'val'
+
+        if not datapath.exists():
+            raise NotImplementedError
 
     args.output_dir = args.output_dir / datapath.name
 
@@ -73,9 +72,7 @@ def train(args: Namespace, modelpath, datapath) -> None:
 
     model_without_ddp = utils.load_model(model_without_ddp,args) # If eval_only, optimizer will not be loaded (only relevant for training)
 
-
     folderpaths = [folderpath for folderpath in sorted(datapath.iterdir()) if re.findall('\d\d$',folderpath.name)]
-    folderpaths = [fp for fp in folderpaths if int(fp.stem) in [10]]
 
     if not args.tracking or not args.masks:
         raise NotImplementedError
@@ -104,8 +101,12 @@ def train(args: Namespace, modelpath, datapath) -> None:
     total_time = time.time() - start_time
     fps = total_frames / total_time
 
-    # with open(str(args.output_dir.parent / 'FPS.txt'), 'w') as file:
-    #     file.write(f"Frames per second (FPS): {fps:2f}\n")
+    with open(str(args.output_dir.parent / 'FPS.txt'), 'w') as file:
+        file.write(f"Frames per second (FPS): {fps:2f}\n")
+
+@ex.config
+def my_config():
+    dataset = yaml_files[0]  # Default dataset
 
 @ex.main
 def load_config(_config, _run):
@@ -113,6 +114,16 @@ def load_config(_config, _run):
     sacred.commands.print_config(_run)
 
 if __name__ == '__main__':
-    config = ex.run_commandline().config
-    args = nested_dict_to_namespace(config)
-    train(args,modelpath,datapath)
+    args = ex.run_commandline().config
+    dataset = args['dataset']
+    respath = filepath.parents[1] / 'results' / dataset
+
+    ex.add_config(str(respath / 'config.yaml'))
+    args = ex.run_commandline().config
+    args = utils.nested_dict_to_namespace(args)
+
+    args.output_dir = Path(args.output_dir)
+    args.data_dir = Path(args.data_dir)
+    datapath = args.data_dir / dataset / 'CTC' / 'test'
+
+    train(args,datapath)
